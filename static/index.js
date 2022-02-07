8715412d63d12d19ad47fd075223f06233160f7d
pixelready <bizartist@gmail.com>
2022-02-07 15:15:29 -0800

    added script for listing cupcakes on homepage

--- static/index.js
+++ static/index.js
@@ -1,2 +1,23 @@
 "use strict";
 
+async function getAllCupcakes() {
+  const response = await axios.get("/api/cupcakes");
+  return response.data.cupcakes;
+}
+
+async function displayCupcakesList() {
+  const cupcakes = await getAllCupcakes();
+
+  for (let cupcake of cupcakes) {
+    const cupcakeTemplate = `
+        <li>
+            <img src ="${cupcake.image}" />
+            <span>Flavor: ${cupcake.flavor}</span>
+            <span>Size: ${cupcake.size}</span>
+            <span>Rating: ${cupcake.rating}</span>
+        </li>`;
+    $("#cupcake-list").append(cupcakeTemplate);
+  }
+}
+
+$(document).ready(displayCupcakesList);
